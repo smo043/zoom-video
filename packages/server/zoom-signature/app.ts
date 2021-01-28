b872ef5502c03ff7f98dc53ebf4e8700b3292322
Salman Mohammed <msalman@sapient.com>
2021-01-28 11:51:07 -0500

    Zoom signature changes added

--- packages/server/zoom-signature/app.ts
+++ packages/server/zoom-signature/app.ts
@@ -6,12 +6,15 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json(), cors());
-app.options('*', cors());
+// app.options('*', cors());
 
 app.post('/', (req: Request, res: Response) => {
   const timestamp = new Date().getTime() - 30000;
   const msg = Buffer.from(process.env.API_KEY + req.body.meetingNumber + timestamp + req.body.role).toString('base64');
-  const hash = crypto.createHmac('sha256', process.env.API_SECRET).update(msg).digest('base64');
+  const hash = crypto
+    .createHmac('sha256', process.env.API_SECRET as string)
+    .update(msg)
+    .digest('base64');
   const signature = Buffer.from(
     `${process.env.API_KEY}.${req.body.meetingNumber}.${timestamp}.${req.body.role}.${hash}`
   ).toString('base64');
