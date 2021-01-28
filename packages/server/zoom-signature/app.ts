d119ec73bc6b75fa802824a85eb357384a187f17
Salman Mohammed <msalman@sapient.com>
2021-01-28 12:15:16 -0500

    web signature route added

--- packages/server/zoom-signature/app.ts
+++ packages/server/zoom-signature/app.ts
@@ -4,24 +4,29 @@ import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 4000;
+const API_KEY = process.env.API_KEY;
+const API_SECRET = process.env.API_SECRET;
 
 app.use(express.json(), cors());
-// app.options('*', cors());
 
-app.post('/', (req: Request, res: Response) => {
+app.post('/web-signature', (req: Request, res: Response) => {
+  const {role, meetingNumber} = req.body;
   const timestamp = new Date().getTime() - 30000;
-  const msg = Buffer.from(process.env.API_KEY + req.body.meetingNumber + timestamp + req.body.role).toString('base64');
-  const hash = crypto
-    .createHmac('sha256', process.env.API_SECRET as string)
-    .update(msg)
-    .digest('base64');
-  const signature = Buffer.from(
-    `${process.env.API_KEY}.${req.body.meetingNumber}.${timestamp}.${req.body.role}.${hash}`
-  ).toString('base64');
+  const msg = Buffer.from(API_KEY + meetingNumber + timestamp + role).toString('base64');
+  const hash = crypto.createHmac('sha256', API_SECRET).update(msg).digest('base64');
+  const signature = Buffer.from(`${API_KEY}.${meetingNumber}.${timestamp}.${role}.${hash}`).toString('base64');
 
   res.json({
     signature: signature
   });
 });
 
-app.listen(port, () => console.log(`Zoom Web SDK Signature Node.js on port ${port}!`));
+app.listen(port, () => console.log(`Zoom Web SDK Signature on port ${port}!`));
+
+process.on('uncaughtException', (err) => {
+  console.error(`uncaught exception : ${err}`);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error(`Unhandled Rejection at ${JSON.stringify(promise)} and reason: ${reason}`);
+});
